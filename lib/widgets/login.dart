e37b7a8795ea372f32e05de69b773004f834fb31
= <ekenemadunagu@gmail.com>
2019-03-05 04:23:09 +0100

    Initial commit

---
+++ lib/widgets/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(widget.title)),
+        body: Text('stay here for now'));
+  }
+}
