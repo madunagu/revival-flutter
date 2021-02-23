3c81903f289a08efbe5a672476a27c8402c82023
Ekene Madunagu <ekenemadunagu@gmail.com>
2021-02-23 15:23:25 +0100

    multiple image posting in sheet

--- lib/sheets/ImageSheet.dart
+++ lib/sheets/ImageSheet.dart
@@ -1,11 +1,14 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 
 import 'package:devotion/bloc/blocs/form.bloc.dart';
 import 'package:devotion/bloc/events/FormEvent.dart';
 import 'package:devotion/bloc/states/FormSheetState.dart';
+import 'package:devotion/misc/StyleConstants.dart';
 import 'package:devotion/models/ResizedImage.dart';
 import 'package:devotion/util/NetworkingClass.dart';
+import 'package:devotion/widgets/FABWidget.dart';
 import 'package:devotion/widgets/SelectImageWidget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -19,102 +22,106 @@ class ImageSheet extends StatefulWidget {
 class _ImageSheetState extends State<ImageSheet> {
   final ResizedImage myImage = ResizedImage();
   final NetworkingClass myNetwork = NetworkingClass();
-
+  List<File> images = <File>[];
+//  List<SelectImageWidget> selectImageWidgets = <SelectImageWidget>[];
   final _formKey = GlobalKey<FormState>();
+
+  Future<File> _imgFromGallery() async {
+    PickedFile image = await ImagePicker()
+        .getImage(source: ImageSource.gallery, imageQuality: 50);
+
+    return File(image.path);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: BlocBuilder<FormBloc, FormSheetState>(
-        builder: (BuildContext context, FormSheetState state) {
-          return Container(
-            color: Colors.white,
-            padding: EdgeInsets.symmetric(horizontal: 40),
-            child: BlocListener<FormBloc, FormSheetState>(
-              listener: (context, state) {
-                print(state);
-                if (state is FormSuccess) {
-                  log(state.toString());
-                  Navigator.of(context)
-                      .pop(ResizedImage.fromJson(state.object['data']));
-                }
-              },
-              // condition: (state, state2) {
-              //   if (state is FormSuccess) {
-              //     return true;
-              //   } n
-              //   return false;
-              // },
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 44,
-                    ),
-                    Text(
-                      'Upload Image',
-                      style:
-                          TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
-                    ),
-                    SizedBox(
-                      height: 21,
-                    ),
-                    SelectImageWidget(
-                      title: 'Image',
-                      state: state,
-                      input: 'image',
-                      onTap: (){
-                        setState(() {
-
-                        });
-                      }
-                    ),
-                    SizedBox(height: 20),
-                    GestureDetector(
-                      onTap:
-                          state is FormInProgress ? null : submitButtonPressed,
-                      child: Container(
-                        height: 52,
-                        width: double.infinity,
-                        padding: EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(82),
-                          color: state is FormInProgress
-                              ? Colors.grey
-                              : Color(0xff8a56ac),
-                        ),
-                        child: Center(
-                          child: Text(
-                            'CONTINUE',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
+    Size size = MediaQuery.of(context).size;
+    return BlocBuilder<FormBloc, FormSheetState>(
+      builder: (BuildContext context, FormSheetState state) {
+        return Stack(
+          children: [
+            Container(
+              height: size.height - 200,
+              width: size.width,
+              color: trendingColors[2],
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: BlocListener<FormBloc, FormSheetState>(
+                listener: (context, state) {
+                  print(state);
+                  if (state is FormSuccess) {
+                    log(state.toString());
+                    Navigator.of(context).pop(state.object['data']);
+                  }
+                },
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 44,
+                      ),
+                      Text(
+                        'Upload Image',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
                         ),
                       ),
-                    ),
-                    SizedBox(height: 20),
-                  ],
+                      SizedBox(
+                        height: 21,
+                      ),
+                      for (var imageFile in images)
+                        SelectedImageWidget(
+                          imageFile,
+                          onTap: () {
+                            images.remove(imageFile);
+                          },
+                        ),
+                      SizedBox(height: 20),
+                    ],
+                  ),
                 ),
               ),
             ),
-          );
-        },
-      ),
+            Positioned(
+              bottom: 0,
+              child: GestureDetector(
+                onTap: state is FormInProgress ? null : submitButtonPressed,
+                child: Container(
+                  height: 52,
+                  width: double.infinity,
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(82),
+                    color: state is FormInProgress
+                        ? Colors.grey
+                        : Color(0xff8a56ac),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'CONTINUE',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            FABWidget(
+              icon: Icons.attach_file,
+              onTap: () async {
+                images.add(await _imgFromGallery());
+                setState(() {});
+              },
+            ),
+          ],
+        );
+      },
     );
   }
-
-  String validationMessage(FormSheetState state, String inputName) {
-    if (state is FormInvalidated) {
-      if (state.errors.containsKey(inputName)) {
-        return state.errors[inputName][0];
-      }
-    }
-    return null;
-  }
-
-
 
   @override
   void initState() {
@@ -127,9 +134,14 @@ class _ImageSheetState extends State<ImageSheet> {
   }
 
   void submitButtonPressed() {
+    Map<String, List<dynamic>> obj = {'photos': []};
+    for (File image in images) {
+      obj['photos'].add(base64Encode(image.readAsBytesSync()));
+    }
+
     BlocProvider.of<FormBloc>(context).add(
       CreateButtonPressed(
-        object: myImage.toJson(),
+        object: obj,
         url: '/images',
       ),
     );
@@ -139,5 +151,25 @@ class _ImageSheetState extends State<ImageSheet> {
     setState(() {
       // this.myAddress.property
     });
+  }
+}
+
+class SelectedImageWidget extends StatelessWidget {
+  SelectedImageWidget(this.file, {this.onTap});
+  final File file;
+  final VoidCallback onTap;
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(60),
+          bottomLeft: Radius.circular(60),
+        ),
+        child: Column(
+          children: [
+            InkWell(onTap: onTap, child: Icon(Icons.remove_circle_outline)),
+            Image.file(file, fit: BoxFit.cover),
+          ],
+        ));
   }
 }
